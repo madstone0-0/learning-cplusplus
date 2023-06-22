2e0e9ac78ac74b8fccde57e474448f06fb0dc7e9
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-06-22 21:49:44 +0000

    (bytes): Fucking around

--- src/misc/bytes.cpp
+++ src/misc/bytes.cpp
@@ -4,10 +4,16 @@
 using std::cout, std::cin, std::string;
 
 int main(int argc, char *argv[]) {
-    cout << "Baller\n";
-    for (auto i = 1; i < argc; i++) {
-        cout << argv[i] << '\n';
+    string input{""};
+    if (argc == 0) {
+        cout << "Please enter value:\n";
+        cin >> input;
+    } else {
+        input = argv[1];
+        input.append(argv[2]);
     }
+
+    cout << input << "\n";
 
     return 0;
 }
