5498f3fae23de4367c6b902d24574ad6aa679030
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-08-14 14:56:35 +0000

    Added multithreading to mgrep

--- src/programs/mgrep.cpp
+++ src/programs/mgrep.cpp
@@ -1,11 +1,18 @@
 #include <algorithm>
 #include <boost/algorithm/searching/boyer_moore.hpp>
 #include <boost/algorithm/string/case_conv.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 #include <boost/program_options.hpp>
+#include <condition_variable>
 #include <exception>
 #include <filesystem>
 #include <fstream>
+#include <future>
 #include <iostream>
+#include <iterator>
+#include <map>
+#include <mutex>
 #include <regex>
 #include <sstream>
 #include <string>
@@ -14,13 +21,48 @@
 
 namespace op = boost::program_options;
 namespace fs = std::filesystem;
+namespace ba = boost::algorithm;
 
 using std::cout, std::endl, std::string, std::cerr, std::vector, std::regex, std::smatch, std::ifstream,
-    std::stringstream, std::regex_search, std::string_view;
+    std::stringstream, std::regex_search, std::string_view, std::map, std::future, std::launch, std::async;
+
+template <typename Pattern>
+using SearchFunctionType = map<fs::path, string> (*)(fs::path, Pattern, std::counting_semaphore<>&);
 
 struct Result {
+    fs::path item;
     bool status;
     string line;
+};
+
+template <typename T>
+bool isReady(const future<T>& f) {
+    if (f.valid()) {
+        return f.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+    } else {
+        return false;
+    }
+}
+
+class Semaphore {
+   public:
+    explicit Semaphore(size_t count) : count{count} {}
+    void lock() {
+        std::unique_lock<std::mutex> lock{mutex};
+        cv.wait(lock, [this] { return count != 0; });
+        count--;
+    }
+
+    void unlock() {
+        std::unique_lock<std::mutex> lock{mutex};
+        count++;
+        cv.notify_one();
+    }
+
+   private:
+    size_t count;
+    std::condition_variable cv;
+    std::mutex mutex;
 };
 
 ifstream open(const fs::path path, std::ios_base::openmode mode = std::ios_base::in) {
@@ -39,74 +81,125 @@ string readFile(ifstream& file) {
     return contentSS.str();
 }
 
-bool searchFile(fs::path item, string pattern) {
-    if (!fs::is_regular_file(item)) return false;
+vector<string> getLines(string corpus) {
+    vector<string> lines;
+    ba::split(lines, corpus, [](auto& c) { return c == '\n'; });
+    return lines;
+}
+
+Result searchFile(fs::path item, string pattern) {
+    if (!fs::is_regular_file(item)) return {fs::path{item}, false, ""};
 
     boost::algorithm::boyer_moore bm{pattern.begin(), pattern.end()};
 
     auto contentStream = open(item);
-    string contentString = readFile(contentStream);
-    std::replace(contentString.begin(), contentString.end(), '\n', ' ');
+    auto contentString = readFile(contentStream);
+    auto lines = getLines(contentString);
 
-    const auto [resBeg, resEnd] = bm(contentString);
-    return resBeg != resEnd;
+    for (const auto& line : lines) {
+        const auto [resBeg, resEnd] = bm(line);
+        if (resBeg != resEnd) {
+            if (line.size() >= 120) {
+                auto res = Result{fs::path{item}, true, string{line.begin(), line.begin() + 120}};
+                return res;
+            }
+            auto res = Result{fs::path{item}, true, line};
+            return res;
+        }
+    }
+
+    return {fs::path{""}, false, ""};
 }
 
-bool searchFile(fs::path item, regex pattern) {
-    if (!fs::is_regular_file(item)) return false;
-    // smatch results;
+Result searchFile(fs::path item, regex pattern) {
+    if (!fs::is_regular_file(item)) return {fs::path{item}, false, ""};
+    smatch regexResults;
 
     auto contentStream = open(item);
     string contentString = readFile(contentStream);
     std::replace(contentString.begin(), contentString.end(), '\n', ' ');
-    const auto result = regex_search(contentString, pattern);
-    return result;
+    const auto result = regex_search(contentString, regexResults, pattern);
+    return {fs::path{item}, result, string{regexResults.str()}};
 }
 
-vector<fs::path> searchDir(fs::path dir, string pattern) {
-    // smatch results;
-    vector<fs::path> paths;
+template <typename Pattern>
+map<fs::path, string> searchDir(fs::path dir, Pattern pattern, Semaphore& maxThreads) {
+    map<fs::path, string> paths;
+    vector<future<Result>> futures;
+    vector<Result> readyFutures;
 
     for (const auto& item : fs::directory_iterator{dir}) {
-        if (searchFile(item, pattern)) {
-            paths.push_back(item.path());
-        }
+        futures.push_back(async(
+            launch::async,
+            [](decltype(maxThreads)& maxThreads, auto item, auto pattern) {
+                std::scoped_lock m(maxThreads);
+                return searchFile(item, pattern);
+            },
+            std::ref(maxThreads), item, pattern));
     }
+
+    do {
+        for (auto& future : futures) {
+            if (isReady(future)) readyFutures.push_back(future.get());
+        }
+    } while (readyFutures.size() < futures.size());
+
+    for (const auto& result : readyFutures) {
+        if (result.status) paths[result.item] = result.line;
+    }
+
     return paths;
 }
 
-vector<fs::path> searchDir(fs::path dir, regex pattern) {
-    // smatch results;
-    vector<fs::path> paths;
-
-    for (const auto& item : fs::directory_iterator{dir}) {
-        if (searchFile(item, pattern)) {
-            paths.push_back(item.path());
-        }
-    }
-    return paths;
-}
-
-vector<fs::path> searchDirRecursively(fs::path dir, string pattern) {
-    vector<fs::path> paths;
+template <typename Pattern>
+map<fs::path, string> searchDirRecursively(fs::path dir, Pattern pattern, Semaphore& maxThreads) {
+    map<fs::path, string> paths;
+    vector<future<Result>> futures;
+    vector<Result> readyFutures;
 
     for (const auto& item : fs::recursive_directory_iterator{dir}) {
-        if (searchFile(item, pattern)) {
-            paths.push_back(item.path());
-        }
+        futures.push_back(async(
+            launch::async,
+            [](decltype(maxThreads)& maxThreads, auto item, auto pattern) {
+                std::scoped_lock m(maxThreads);
+                return searchFile(item, pattern);
+            },
+            std::ref(maxThreads), item, pattern));
     }
+
+    do {
+        for (auto& future : futures) {
+            if (isReady(future)) readyFutures.push_back(future.get());
+        }
+    } while (readyFutures.size() < futures.size());
+
+    for (const auto& result : readyFutures) {
+        if (result.status) paths[result.item] = result.line;
+    }
+
     return paths;
 }
 
-vector<fs::path> searchDirRecursively(fs::path dir, regex pattern) {
-    vector<fs::path> paths;
+template <typename Pattern>
+auto searchWorker(fs::path p, Pattern pattern, Semaphore& maxThreads) {
+    return async(
+        launch::async,
+        [](fs::path p, decltype(maxThreads)& maxThreads, auto pattern) {
+            std::scoped_lock m(maxThreads);
+            return searchDir(p, pattern, maxThreads);
+        },
+        std::move(p), std::ref(maxThreads), pattern);
+}
 
-    for (const auto& item : fs::recursive_directory_iterator{dir}) {
-        if (searchFile(item, pattern)) {
-            paths.push_back(item.path());
-        }
-    }
-    return paths;
+template <typename Pattern>
+auto searchRecWorker(fs::path p, Pattern pattern, Semaphore& maxThreads) {
+    return async(
+        launch::async,
+        [](fs::path p, decltype(maxThreads)& maxThreads, auto pattern) {
+            std::scoped_lock m(maxThreads);
+            return searchDirRecursively(p, pattern, maxThreads);
+        },
+        std::move(p), std::ref(maxThreads), pattern);
 }
 
 int main(int argc, char** argv) {
@@ -154,9 +247,14 @@ int main(int argc, char** argv) {
     }
 
     const auto threads = vm["threads"].as<int>();
-    // const auto& pattern = boost::to_lower_copy(vm["pattern"].as<string>());
-    const auto& pattern = vm["pattern"].as<string>();
+    auto& pattern = vm["pattern"].as<string>();
     const auto& path = vm["path"].as<std::vector<string>>();
+    Semaphore maxThreads{static_cast<size_t>(threads)};
+
+    if (threads < 2) {
+        std::cerr << "Threads used must be at least 2 due to a bug" << endl;
+        return -1;
+    }
 
     regex re;
     try {
@@ -168,46 +266,66 @@ int main(int argc, char** argv) {
         }
     }
 
-    vector<fs::path> paths;
-    try {
-        for (const auto& p : path) {
-            if (!isRegex) {
-                if (fs::is_directory(p) && !isRecursive) {
-                    paths = searchDir(p, pattern);
-                } else if (fs::is_directory(p) && isRecursive) {
-                    paths = searchDirRecursively(p, pattern);
-                } else if (fs::is_regular_file(p)) {
-                    if (!searchFile(p, pattern)) continue;
-                    paths.push_back(p);
-                } else {
-                    cerr << p << " cannot be searched." << endl;
-                    return -1;
-                }
+    map<fs::path, string> paths;
+    vector<future<decltype(paths)>> futures;
+    vector<decltype(paths)> readyFutures;
+
+    for (const auto& p : path) {
+        if (!isRegex) {
+            if (fs::is_directory(p) && !isRecursive) {
+                futures.push_back(searchWorker(fs::path{p}, pattern, maxThreads));
+            } else if (fs::is_directory(p) && isRecursive) {
+                futures.push_back(searchRecWorker(fs::path{p}, pattern, maxThreads));
+            } else if (fs::is_regular_file(p)) {
+                auto searchResults = searchFile(p, pattern);
+                if (!searchResults.status) continue;
+                paths.insert({p, searchResults.line});
             } else {
-                if (fs::is_directory(p) && !isRecursive) {
-                    paths = searchDir(p, re);
-                } else if (fs::is_directory(p) && isRecursive) {
-                    paths = searchDirRecursively(p, re);
-                } else if (fs::is_regular_file(p)) {
-                    if (!searchFile(p, re)) continue;
-                    paths.push_back(p);
-                } else {
-                    cerr << p << " cannot be searched." << endl;
-                    return -1;
-                }
+                cerr << p << " cannot be searched." << endl;
+                return -1;
+            }
+        } else {
+            if (fs::is_directory(p) && !isRecursive) {
+                futures.push_back(searchWorker(fs::path{p}, re, maxThreads));
+            } else if (fs::is_directory(p) && isRecursive) {
+                futures.push_back(searchRecWorker(fs::path{p}, re, maxThreads));
+            } else if (fs::is_regular_file(p)) {
+                auto searchResults = searchFile(p, pattern);
+                if (!searchResults.status) continue;
+                paths.insert({p, searchResults.line});
+            } else {
+                cerr << p << " cannot be searched." << endl;
+                return -1;
             }
         }
+    }
 
+    try {
+        do {
+            for (auto& future : futures) {
+                if (isReady(future)) readyFutures.push_back(future.get());
+            }
+        } while (readyFutures.size() < futures.size());
+
+        for (const auto& results : readyFutures) {
+            paths.insert(results.begin(), results.end());
+        }
     } catch (const std::exception& e) {
         cerr << "Error: " << e.what() << endl;
         return -1;
     }
 
-    cout << "Pattern \"" << pattern << "\" found in the following files:\n";
-    for (const auto& path : paths) cout << "\t" << path.string() << "\n";
-    cout << "\n";
+    if (paths.size() > 1) {
+        for (const auto& [path, line] : paths) {
+            const auto displayPath = path.string().size() >= 120 ? path.filename().string() : path.string();
+            cout << displayPath << ":" << line << "\n";
+        }
+    } else if (paths.size() == 1) {
+        auto [path, line] = *(paths.begin());
+        const auto displayPath = path.string().size() >= 120 ? path.filename().string() : path.string();
 
-    cout << "OK" << endl;
+        cout << displayPath << ":" << line << "\n";
+    }
 
     return 0;
 }
